0294f7546a7ccebc744afbd630996f8e128b97ca
AntonioSkopin <askopin@outlook.com>
2020-08-02 00:51:40 +0200

    Added Model class and DB context class

---
+++ KapperAPI/Models/Afspraak.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Afspraak
+    {
+        [Required]
+        public int AfspraakID { get; set; }
+        
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string KlantNaam { get; set; }
+        
+        [Required]
+        [StringLength(10, ErrorMessage = "Telefoon nummer moet 10 nummers bevatten.")]
+        public string KlantTelnr { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime AfspraakDatum { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        public TimeSpan AfspraakTijd { get; set; }
+    }
+}
---
+++ KapperAPI/Models/KapperContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    public class KapperContext : DbContext
+    {
+        public KapperContext(DbContextOptions<KapperContext> options) : base(options) { }
+        public DbSet<Afspraak> Afspraak { get; set; }
+    }
+}
